74f86986d5138a9ebd7fdc7817e0d5492b21cf7b
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-01-01 23:17:28 -0800

    rechart

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,3 +5,8 @@ export const useDarkMode = (initialValue) => {
 
     return [value, setValue];
 }
+
+//non visual behavior
+//business logic
+//stateful logic
+//happens behind the scenes that has nothing to do with the UI. ends up affecting UI but doesnt return JSX or classes
